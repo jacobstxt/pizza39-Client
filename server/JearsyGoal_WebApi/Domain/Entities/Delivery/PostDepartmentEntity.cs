01cd914835fb801a108e83836dbea5dcd7e37e3b
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-08-03 12:10:44 +0300

    add server

---
+++ server/JearsyGoal_WebApi/Domain/Entities/Delivery/PostDepartmentEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Delivery
+{
+    [Table("tbl_PostDepartments")]
+    public class PostDepartmentEntity: BaseEntity<long>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [ForeignKey("City")]
+        public long CityId { get; set; }
+        public CityEntity? City { get; set; }
+
+        public ICollection<DeliveryInfoEntity>? DeliveryInfos { get; set; }
+    }
+}
